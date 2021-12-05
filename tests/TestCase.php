412021274b7c687092c8e0c5016c964492a71c09
Gustavo Willer <gustavo.willer@hotmail.com>
2021-12-05 20:38:20 -0300

    feat: config test to database memory

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,17 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function initDatabase()
+    {
+        if (DB::getDatabaseName() === ':memory:') {
+            Artisan::call('migrate');
+        }
+    }
 }
